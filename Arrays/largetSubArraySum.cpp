b976691e2b3bb6b5b3a7efb3017c55d74e73c9fe
Manish Jaryal <manishjaryal51@gmail.com>
2026-03-20 10:18:17 +0530

    largest subArray sum

---
+++ Arrays/largetSubArraySum.cpp
@@ -0,0 +1,54 @@
+/*
+Given an integer array nums, find the subarray with the largest sum and return the sum of the elements present in that subarray.
+
+
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+
+Example 1
+
+Input: nums = [2, 3, 5, -2, 7, -4]
+
+Output: 15
+
+Explanation:
+
+The subarray from index 0 to index 4 has the largest sum = 15
+
+Example 2
+
+Input: nums = [-2, -3, -7, -2, -10, -4]
+
+Output: -2
+
+Explanation:
+
+The element on index 0 or index 3 make up the largest sum when taken as a subarray
+*/
+
+#include<iostream>
+#include <vector>
+#include<climits>
+using namespace std;
+int maxSubArray(vector<int>& nums) {
+     int sum=0;
+     int max=INT_MIN;
+     for(int i=0;i<nums.size();i++){
+        sum=sum+nums[i];
+      
+        if(sum>max){
+            max=sum;
+        }
+          if(sum<0){
+            sum=0;
+        }
+        
+     }
+return max;
+    }
+int main(){
+
+
+return 0;
+}
